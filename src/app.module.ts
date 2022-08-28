928e1aebed3a4dae78c98888d82afafe664cdf17
Juan Cruz Gonzalez <juancruz.gonfer@gmail.com>
2022-08-28 19:18:37 -0300

    section 3 finish

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CarsModule } from './cars/cars.module';
 
 @Module({
-  imports: [],
+  imports: [CarsModule],
   controllers: [AppController],
   providers: [AppService],
 })
